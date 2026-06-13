2618c57d55850767ef9829c33e8f45946ee06f42
Jolfreilyn Amancio <jofrychat@gmail.com>
2026-06-12 22:03:58 -0400

    primer commit

---
+++ app.py
@@ -0,0 +1,10 @@
+# hola_mundo.py
+def main():
+    print("¡Hola Mundo desde Python!")
+    print("Mensaje de Integración Continua:")
+    print("Estudiante: Jolfreilyn Amancio")
+    print("ID: 2025-1060")
+    print("CI/CD Pipeline ejecutado con éxito. 🚀")
+
+if __name__ == "__main__":
+    main()
